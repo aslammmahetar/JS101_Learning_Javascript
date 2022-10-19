8a181cb4ff498de39a68ef451131377a1b937bba
aslammmahetar <115460260+aslammmahetar@users.noreply.github.com>
2022-10-19 12:31:50 +0530

    Day 3 Programme

---
+++ Day 3 programme/problem4.js
@@ -0,0 +1,17 @@
+// Problem 4: Given stored username and password and input username and password, Print if the user can login or not.
+
+let i_username = "official_.nawab._";
+let i_password = "NawabPvt";
+
+let st_username = "nawab_pvt";
+let st_password = "NawabPvt";
+
+if(i_username==st_username){
+    if(i_password==st_password)}
+{
+
+  console.log("Login Successfully")
+}
+else {
+  console.log("Invalid Username Or Password");
+}
